5126e00ba634606f0344c0feb9ee5f0e5722cfc8
vagrant <vagrant@homestead>
2019-10-22 15:11:10 +0000

    daily_reportテーブルの初期値設定

---
+++ database/seeds/DailyReportsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class DailyReportsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('daily_reports')->truncate();
+        DB::table('daily_reports')->insert([
+            [
+                'title'             => 'dailyreportのtitle',
+                'user_id'           => '1',
+                'content'           => 'ここは本文',
+                'reporting_time'    => '20191021',
+                'created_at'        => Carbon::create(2019, 10, 21),
+                'updated_at'        => Carbon::create(2019, 10, 21),
+            ]
+        ]);
+    }
+}
