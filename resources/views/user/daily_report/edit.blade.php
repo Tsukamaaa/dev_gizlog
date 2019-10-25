a81d9539e37a6cc1249e8c5a2bf07993279ca8b2
vagrant <vagrant@homestead>
2019-10-25 04:41:45 +0000

    Form::inputでPostする内容を指定、初期値もとっている

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -4,22 +4,28 @@
 <h1 class="brand-header">日報編集</h1>
 <div class="main-wrap">
   <div class="container">
-    <form>
-      <input class="form-control" name="user_id" type="hidden" value="4">
+    {!! Form::open(['route' => ['daily_report.update', $daily_report->id], 'method' => 'PUT']) !!}
+    <!-- <form> -->
+      <!-- <input class="form-control" name="user_id" type="hidden" value="{{ Auth::id() }}"> -->
       <div class="form-group form-size-small">
-        <input class="form-control" name="reporting_time" type="date">
+        {!! Form::input('date', 'reporting_time', date('Y-m-j'), ['required', 'class' => 'form-control']) !!}
+        <!-- <input class="form-control" name="reporting_time" type="date"> -->
       <span class="help-block"></span>
       </div>
       <div class="form-group">
-        <input class="form-control" placeholder="Title" name="title" type="text">
+        {!! Form::input('text', 'title', $daily_report->title, ['required',  'class' => 'form-control'] )!!}
+        <!-- <input class="form-control" placeholder="Title" name="title" type="text"> -->
       <span class="help-block"></span>
       </div>
       <div class="form-group">
-        <textarea class="form-control" placeholder="本文" name="contents" cols="50" rows="10">本文</textarea>
+        {!! Form::textarea('content', $daily_report->content, ['required', 'class' => 'form-control']) !!}
+        <!-- <textarea class="form-control" placeholder="本文" name="contents" cols="50" rows="10">本文</textarea> -->
       <span class="help-block"></span>
       </div>
-      <button type="submit" class="btn btn-success pull-right">Update</button>
-    </form>
+      {!! Form::submit('Update', ['class' => 'btn btn-success pull-right']) !!}
+      <!-- <button type="submit" class="btn btn-success pull-right">Update</button> -->
+    <!-- </form> -->
+    {!! Form::close() !!}
   </div>
 </div>
 
