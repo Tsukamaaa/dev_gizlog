9b2e4d464777078ff8e675c773c6d94b30cd1999
vagrant <vagrant@homestead>
2019-10-27 14:31:54 +0000

    validationに引っかかった時のメッセージが表示されるようにした

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -6,17 +6,37 @@
   <div class="container">
     {!! Form::open(['route' => 'daily_report.store']) !!}
       <div class="form-group form-size-small">
-        {!! Form::input('date', 'reporting_time', date('Y-m-j'), ['class' => 'form-control']) !!}
-        <span class="help-block"></span>
+        {!! Form::input('date','reporting_time', date('Y-m-j'), ['class' => "form-control {{ $errors->has('reporting_time') ? ' is-invalid' : '' }}" ]) !!}
+
+        @if ($errors->has('reporting_time'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('reporting_time') }}</strong>
+          </span>
+        @endif
+
       </div>
+
       <div class="form-group">
-        {!! Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' =>'Title']) !!}
-        <span class="help-block"></span>
+        {!! Form::input('text', 'title', null, ['class' => "form-control {{ $errors->has('title') ? 'in-valid' : '' }}", 'placeholder' =>'Title']) !!}
+        
+        @if ($errors->has('title'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('title') }}</strong>
+          </span>
+        @endif
+
       </div>
+
       <div class="form-group">
-        {!! Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
-        <span class="help-block"></span>
+        {!! Form::textarea('content', null, ['class' => "form-control {{ $errors->has('content') ? 'in-valid' : '' }}", 'placeholder' => 'Content']) !!}
+        
+        @if ($errors->has('content'))
+          <span class="help-block">
+            <strong>{{ $errors->first('content') }}</strong>
+          </span>
+        @endif
       </div>
+      
       {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
     {!! Form::close() !!}
   </div>
