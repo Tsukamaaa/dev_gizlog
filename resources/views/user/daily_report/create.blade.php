4c1bda5d59757ac377a4b1dbb73daebb4e7a6c0d
vagrant <vagrant@homestead>
2019-10-28 14:06:08 +0000

    validation発火時のclassの追加でcssを付与

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,38 +5,52 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => 'daily_report.store']) !!}
-      <div class="form-group form-size-small">
-        {!! Form::input('date','reporting_time', date('Y-m-j'), ['class' => "form-control {{ $errors->has('reporting_time') ? ' is-invalid' : '' }}" ]) !!}
 
-        @if ($errors->has('reporting_time'))
-          <span class="help-block" role="alert">
-            <strong>{{ $errors->first('reporting_time') }}</strong>
-          </span>
-        @endif
+      @if ($errors->has('reporting_time'))
+        <div class="form-group form-size-small has-error">
+      @else
+        <div class="form-group form-size-small">
+      @endif
 
-      </div>
+          {!! Form::input('date','reporting_time', date('Y-m-j'), ['class' => "form-control {{ $errors->has('reporting_time') ? ' is-invalid' : '' }}" ]) !!}
 
-      <div class="form-group">
-        {!! Form::input('text', 'title', null, ['class' => "form-control {{ $errors->has('title') ? 'in-valid' : '' }}", 'placeholder' =>'Title']) !!}
-        
-        @if ($errors->has('title'))
-          <span class="help-block" role="alert">
-            <strong>{{ $errors->first('title') }}</strong>
-          </span>
-        @endif
+          @if ($errors->has('reporting_time'))
+            <span class="help-block" role="alert">
+              <strong>{{ $errors->first('reporting_time') }}</strong>
+            </span>
+          @endif
+        </div>
 
-      </div>
+      @if ($errors->has('title'))
+        <div class="form-group has-error">
+      @else
+        <div class="form-group">
+      @endif
 
-      <div class="form-group">
-        {!! Form::textarea('content', null, ['class' => "form-control {{ $errors->has('content') ? 'in-valid' : '' }}", 'placeholder' => 'Content']) !!}
-        
-        @if ($errors->has('content'))
-          <span class="help-block">
-            <strong>{{ $errors->first('content') }}</strong>
-          </span>
-        @endif
-      </div>
-      
+          {!! Form::input('text', 'title', null, ['class' => "form-control {{ $errors->has('title') ? 'in-valid' : '' }}", 'placeholder' =>'Title']) !!}
+
+          @if ($errors->has('title'))
+            <span class="help-block" role="alert">
+              <strong>{{ $errors->first('title') }}</strong>
+            </span>
+          @endif
+        </div>
+
+      @if ($errors->has('content'))
+        <div class="form-group has-error">
+      @else
+        <div class="form-group">
+      @endif
+
+          {!! Form::textarea('content', null, ['class' => "form-control {{ $errors->has('content') ? 'in-valid' : '' }}", 'placeholder' => 'Content']) !!}
+
+          @if ($errors->has('content'))
+            <span class="help-block" role="alert">
+              <strong>{{ $errors->first('content') }}</strong>
+            </span>
+          @endif
+        </div>
+
       {!! Form::submit('Add', ['class' => 'btn btn-success pull-right']) !!}
     {!! Form::close() !!}
   </div>
