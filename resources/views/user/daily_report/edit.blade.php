77be8b34201823bc8b524a29c8ee434286584987
vagrant <vagrant@homestead>
2019-10-30 08:28:13 +0000

    hass-errorクラスを三項演算子を用いて追加

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -5,51 +5,32 @@
 <div class="main-wrap">
   <div class="container">
     {!! Form::open(['route' => ['daily_report.update', $dailyReport->id], 'method' => 'PUT']) !!}
+      <div class="form-group form-size-small {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
+        {!! Form::input('date', 'reporting_time', $dailyReport->reporting_time->format('Y-m-d'), ['class' => "form-control {{ $errors->has('reporting_time') ? ' is-invalid' : '' }}" ]) !!}
+        @if ($errors->has('reporting_time'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('reporting_time') }}</strong>
+          </span>
+        @endif
+      </div>
 
-      @if ($errors->has('reporting_time'))
-        <div class="form-group form-size-small has-error">
-      @else
-        <div class="form-group form-size-small">
-      @endif
+      <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
+        {!! Form::text('title', $dailyReport->title, ['class' => "form-control {{ $errors->has('title') ? 'in-valid' : '' }}", 'placeholder' =>'Title']) !!}
+        @if ($errors->has('title'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('title') }}</strong>
+          </span>
+        @endif
+      </div>
 
-          {!! Form::input('date', 'reporting_time', $dailyReport->reporting_time->format('Y-m-d'), ['class' => "form-control {{ $errors->has('reporting_time') ? ' is-invalid' : '' }}" ]) !!}
-
-          @if ($errors->has('reporting_time'))
-            <span class="help-block" role="alert">
-              <strong>{{ $errors->first('reporting_time') }}</strong>
-            </span>
-          @endif
-        </div>
-
-      @if ($errors->has('title'))
-        <div class="form-group has-error">
-      @else
-        <div class="form-group">
-      @endif
-
-          {!! Form::input('text', 'title', $dailyReport->title, ['class' => "form-control {{ $errors->has('title') ? 'in-valid' : '' }}", 'placeholder' =>'Title']) !!}
-
-          @if ($errors->has('title'))
-            <span class="help-block" role="alert">
-              <strong>{{ $errors->first('title') }}</strong>
-            </span>
-          @endif
-        </div>
-
-      @if ($errors->has('content'))
-        <div class="form-group has-error">
-      @else
-        <div class="form-group">
-      @endif
-
-          {!! Form::textarea('content', $dailyReport->content, ['class' => "form-control {{ $errors->has('content') ? 'in-valid' : '' }}", 'placeholder' => 'Content']) !!}
-        
-          @if ($errors->has('content'))
-            <span class="help-block" role="alert">
-              <strong>{{ $errors->first('content') }}</strong>
-            </span>
-          @endif
-        </div>
+      <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
+        {!! Form::textarea('content', $dailyReport->content, ['class' => "form-control {{ $errors->has('content') ? 'in-valid' : '' }}", 'placeholder' => 'Content']) !!}
+        @if ($errors->has('content'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('content') }}</strong>
+          </span>
+        @endif
+      </div>
 
       {!! Form::submit('Update', ['class' => 'btn btn-success pull-right']) !!}
     {!! Form::close() !!}
