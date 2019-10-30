6293cc9c6f40a471d70d4917b6ccfc9c0c6cc42f
vagrant <vagrant@homestead>
2019-10-30 04:49:43 +0000

    初期値の追加とCarbon::yesterday()で昨日の日付が入るようにした

--- database/seeds/DailyReportsTableSeeder.php
+++ database/seeds/DailyReportsTableSeeder.php
@@ -21,6 +21,20 @@ class DailyReportsTableSeeder extends Seeder
                 'reporting_time'    => '20191021',
                 'created_at'        => Carbon::create(2019, 10, 21),
                 'updated_at'        => Carbon::create(2019, 10, 21),
+            ],[
+                'title'             => 'Seederのテスト',
+                'user_id'           => '4',
+                'content'           => '多めにテストデータを用意しておく',
+                'reporting_time'    => '20190821',
+                'created_at'        => Carbon::create(2019, 04, 21),
+                'updated_at'        => Carbon::create(2019, 10, 21),
+            ],[
+                'title'             => '日付のゼロ埋めはできてないのか？',
+                'user_id'           => '4',
+                'content'           => 'Carbon::create()の話なのか',
+                'reporting_time'    => Carbon::yesterday(),
+                'created_at'        => Carbon::create(2019, 04, 01),
+                'updated_at'        => Carbon::create(2019, 05, 01),
             ]
         ]);
     }
