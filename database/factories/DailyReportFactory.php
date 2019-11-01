441ae83f45e2dbbb7b4f0ff113f8388e197ed0cb
vagrant <vagrant@homestead>
2019-11-01 03:49:37 +0000

    seederにテストデータを複製して格納するための処理が書かれている

---
+++ database/factories/DailyReportFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\DailyReport::class, function (Faker $faker) {
+    return [
+        'user_id' => $faker->numberBetween($min = 1, $max = 5),
+        'reporting_time' => $faker->dateTime($max = 'now', $timezone = date_default_timezone_get()),
+        'title' => $faker->name,
+        'content' => $faker->realText($maxNbChar = 100, $indexSize = 1)
+        // 'book_name' => $faker->realText($maxNbChars = 20, $indexSize = 1),
+        // numberBetween($min = 1, $max = 100),
+    ];
+});
