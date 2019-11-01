24949094102d1ded93405c3f303a27531f4806a5
vagrant <vagrant@homestead>
2019-11-01 03:59:50 +0000

    returnの配列内を見やすく整えた

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -4,9 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\DailyReport::class, function (Faker $faker) {
     return [
-        'user_id' => $faker->numberBetween($min = 1, $max = 5),
-        'reporting_time' => $faker->dateTime($max = 'now', $timezone = date_default_timezone_get()),
-        'title' => $faker->name,
-        'content' => $faker->realText($maxNbChar = 100, $indexSize = 1)
+        'user_id'           => $faker->numberBetween($min = 1, $max = 5),
+        'reporting_time'    => $faker->dateTime($max = 'now', $timezone = date_default_timezone_get()),
+        'title'             => $faker->name,
+        'content'           => $faker->realText($maxNbChar = 100, $indexSize = 1)
     ];
 });
