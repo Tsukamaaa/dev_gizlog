446e949ad046fa2d5fb146f5f2c1473400bb24e3
vagrant <vagrant@homestead>
2019-11-01 09:19:59 +0000

    Controllerの検索処理をModelに移した

--- app/Models/DailyReport.php
+++ app/Models/DailyReport.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Auth;
 
 class DailyReport extends Model
 {
@@ -18,4 +19,13 @@ class DailyReport extends Model
         'title',
         'content'
     ];
+
+    public function getDailyReport($data) {
+        $query = DailyReport::query()->where('user_id', Auth::id());
+        if (!empty($data->get('search-month'))) {
+            $query->where('reporting_time', 'LIKE', $data->get('search-month'). '%');
+        }
+
+        return $dailyReports = $query->latest()->get();
+    }
 }
