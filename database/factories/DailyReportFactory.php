a525671e25e79adc92eac82fdf9aa987bce67fe7
vagrant <vagrant@homestead>
2019-11-01 03:50:20 +0000

    コメントアウトを削除した

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -8,7 +8,5 @@ $factory->define(App\Models\DailyReport::class, function (Faker $faker) {
         'reporting_time' => $faker->dateTime($max = 'now', $timezone = date_default_timezone_get()),
         'title' => $faker->name,
         'content' => $faker->realText($maxNbChar = 100, $indexSize = 1)
-        // 'book_name' => $faker->realText($maxNbChars = 20, $indexSize = 1),
-        // numberBetween($min = 1, $max = 100),
     ];
 });
