362c4fba2129286f2dee9e234bd66e450aa5ae97
vagrant <vagrant@homestead>
2019-11-01 04:18:58 +0000

    titleとcontentに文字数制限をかけた

--- database/migrations/2019_10_22_142920_create_daily_reports_table.php
+++ database/migrations/2019_10_22_142920_create_daily_reports_table.php
@@ -16,8 +16,8 @@ class CreateDailyReportsTable extends Migration
         Schema::create('daily_reports', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
-            $table->string('title');
-            $table->string('content');
+            $table->string('title', 30);
+            $table->string('content', 1000);
             $table->date('reporting_time');
             $table->timestamps();
             $table->softDeletes();
