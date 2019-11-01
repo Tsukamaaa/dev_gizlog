5633315f6d4116bf21cf5a8f72dbdc111e6664f7
vagrant <vagrant@homestead>
2019-11-01 08:07:28 +0000

    不要になったuse Validatorを削除

--- app/Http/Controllers/User/DailyReportController.php
+++ app/Http/Controllers/User/DailyReportController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\DailyReport;
 use Auth;
-use Validator;
 use App\Http\Requests\DailyReportRequest;
 
 class DailyReportController extends Controller
