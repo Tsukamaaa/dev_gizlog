b3333c23346ddf7de2977f318c9ddf6b7f4f9a44
vagrant <vagrant@homestead>
2019-11-11 04:24:39 +0000

    DailyReportをuseして使用できるように変更

--- database/factories/DailyReportFactory.php
+++ database/factories/DailyReportFactory.php
@@ -1,8 +1,9 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Models\DailyReport;
 
-$factory->define(App\Models\DailyReport::class, function (Faker $faker) {
+$factory->define(DailyReport::class, function (Faker $faker) {
     return [
         'user_id'           => $faker->numberBetween(1, 5),
         'reporting_time'    => $faker->dateTime('now', date_default_timezone_get()),
