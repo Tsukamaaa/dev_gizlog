6c69a644e3efc6cf1c9e5eaeac0de0c3e8855f5d
vagrant <vagrant@homestead>
2019-11-19 13:11:48 +0000

    $fillableとsoftDeleteを追加

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
-    //
+    use softDeletes;
+    protected $dates = [
+        //いったん保留、defaultの日付の形を決めるかどうか
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'tag_category_id',
+        'title',
+        'content'
+    ];
 }
 
