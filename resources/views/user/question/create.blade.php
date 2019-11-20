f26ef6fb28a68560c93f8c4a5c1ce0de6d4bcb9b
vagrant <vagrant@homestead>
2019-11-20 14:12:07 +0000

    作成した情報をFormファサードで送れるように変更

--- resources/views/user/question/create.blade.php
+++ resources/views/user/question/create.blade.php
@@ -4,24 +4,33 @@
 <h2 class="brand-header">質問投稿</h2>
 <div class="main-wrap">
   <div class="container">
-    <form>
+    {!! Form::open(['route' => 'question.store']) !!}
       <div class="form-group">
-        <select name='tag_category_id' class = "form-control selectpicker form-size-small" id="pref_id">
+        {!! Form::select('tag_category_id', ['Select category', 'front', 'back', 'infra', 'others'], 'Select category', ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
+        <!-- <select name='tag_category_id' class = "form-control selectpicker form-size-small" id="pref_id">
           <option value="">Select category</option>
-            <option value= ""></option>
-        </select>
-        <span class="help-block"></span>
+            <option value= "1">front</option>
+            <option value= "2">back</option>
+            <option value= "3">infra</option>
+            <option value= "4">others</option>
+        </select> -->
+        <span class="help-block"></span>  <!--ここはバリデーションエラー-->
       </div>
+
       <div class="form-group">
-        <input class="form-control" placeholder="title" name="title" type="text">
-        <span class="help-block"></span>
+        {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'title']) !!}
+        <!-- <input class="form-control" placeholder="title" name="title" type="text"> -->
+        <span class="help-block"></span>  <!--ここはバリデーションエラー-->
       </div>
+
       <div class="form-group">
-        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10"></textarea>
-        <span class="help-block"></span>
+        {!! Form::textarea('content', null, ['class' => 'form-control', 'cols' => '50', 'rows' => '10', 'placeholder' => 'Please write down your question here...']) !!}
+        <!-- <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10"></textarea> -->
+        <span class="help-block"></span>  <!--ここはバリデーションエラー-->
       </div>
-      <input name="confirm" class="btn btn-success pull-right" type="submit" value="create">
-    </form>
+      {!! Form::submit('CREATE', ['class' => 'btn btn-success pull-right']) !!}
+      <!-- <input name="confirm" class="btn btn-success pull-right" type="submit" value="create"> -->
+    {!! Form::close() !!}
   </div>
 </div>
 
