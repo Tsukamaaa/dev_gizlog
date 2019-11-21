001b0ad7d467d7478fcbba4ae75a183f55421c96
vagrant <vagrant@homestead>
2019-11-21 14:20:42 +0000

    softDeleteを実装

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\User;
 
 class Comment extends Model
 {
-    //
+    use softDeletes;
 }
