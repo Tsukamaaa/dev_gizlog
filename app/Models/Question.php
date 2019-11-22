983752ef8c05a6b1ca09d4d2250e00a44bdb3b23
vagrant <vagrant@homestead>
2019-11-22 03:15:44 +0000

    questionからtab_categoryを取得できるように変更

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
+use App\Models\TagCategory;
 
 class Question extends Model
 {
@@ -23,6 +24,11 @@ class Question extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function tag_category()
+    {
+        return $this->belongsTo(TagCategory::class);
     }
 }
 
