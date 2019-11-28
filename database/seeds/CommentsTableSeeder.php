d7f2992f71c302d4822b570b2a87fd0b1be19ac0
vagrant <vagrant@homestead>
2019-11-28 08:28:22 +0000

    コメントの初期値を設定、factoryは使ってない

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Models\Comments;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('comments')->truncate();
+        DB::table('comments')->insert([
+            [
+                'user_id' => 4,
+                'question_id' => 3,
+                'comment' => 'これはCommentテーブルのコメント',
+                'created_at' => Carbon::yesterday(),
+                'updated_at' => Carbon::yesterday()
+            ],
+            [
+                'user_id' => 4,
+                'question_id' => 3,
+                'comment' => 'これはCommentテーブルのコメント',
+                'created_at' => Carbon::yesterday(),
+                'updated_at' => Carbon::yesterday(),
+            ],
+            [
+                'user_id' => 4,
+                'question_id' => 3,
+                'comment' => 'これはCommentテーブルのコメント',
+                'created_at' => Carbon::yesterday(),
+                'updated_at' => Carbon::yesterday(),
+            ],
+            [
+                'user_id' => 4,
+                'question_id' => 3,
+                'comment' => 'これはCommentテーブルのコメント',
+                'created_at' => Carbon::yesterday(),
+                'updated_at' => Carbon::yesterday(),
+            ]
+        ]);
+    }
+}
