f53af9815478e9d2906b49bbcf5b411a30813a00
vagrant <vagrant@homestead>
2019-12-08 13:55:23 +0000

    表示できるようにして、初期値も選択したものを表示するよう変更

--- resources/views/user/question/edit.blade.php
+++ resources/views/user/question/edit.blade.php
@@ -5,26 +5,37 @@
 
 <div class="main-wrap">
   <div class="container">
-    <form>
-      <div class="form-group">
-        <select name='tag_category_id' class = "form-control selectpicker form-size-small" id ="pref_id">
-          <option value=""></option>
-            <option value= ""></option>
-        </select>
-        <span class="help-block"></span>
+    {!! Form::open(['route' => 'question.update']) !!}
+      <div class="form-group @if ($errors->has('tag_category_id')) has-error @endif">
+      {!! Form::select('tag_category_id', ['' => 'Select category', 1 => 'front', 2 => 'back', 3 => 'infra', 4 => 'others'], $question->tag_category_id, ['class' => 'form-control selectpicker form-size-small', 'id' => 'pref_id']) !!}
+        @if ($errors->has('tag_category_id'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('tag_category_id') }}</strong>
+          </span>
+        @endif
       </div>
-      <div class="form-group">
-        <input class="form-control" placeholder="title" name="title" type="text" value="">
-        <span class="help-block"></span>
+
+      <div class="form-group @if ($errors->has('title')) has-error @endif">
+        {!! Form::text('title', $question->title, ['class' => 'form-control']) !!}
+        @if ($errors->has('title'))
+          <span class="help-block" role="alert">
+            <strong>{{ $errors->first('title')}}</strong>
+          </span>
+        @endif
       </div>
-      <div class="form-group">
-        <textarea class="form-control" placeholder="Please write down your question here..." name="content" cols="50" rows="10"></textarea>
-        <span class="help-block"></span>
+
+      <div class="form-group @if ($errors->has('content')) has-error @endif">
+        {!! Form::textarea('content', $question->content, ['class' => 'form-control', 'cols' => '50', 'rows' => '10']) !!}
+        @if ($errors->has('content'))
+          <span class="help-block" role="alert">
+            <strong>{{$errors->first('content')}}</strong>
+          </span>
+        @endif
       </div>
-      <input name="confirm" class="btn btn-success pull-right" type="submit" value="update">
-    </form>
+
+      {!! Form::submit('UPDATE', ['class' => 'btn btn-succes pull-right']) !!}
+    {!! Form::close() !!}
   </div>
 </div>
 
 @endsection
-
